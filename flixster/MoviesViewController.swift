5f29ac0bbc84d46d3c2e53ee61d58bebcb0bd142
Brylee Pavlik <pavlikbr@msu.edu>
2021-02-13 17:19:13 -0500

    Completed Flixster app

---
+++ flixster/MoviesViewController.swift
@@ -0,0 +1,87 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Brylee Pavlik on 2/13/21.
+//
+
+import UIKit
+import AlamofireImage
+
+//need to add UITableViewDataSource and UITableViewDelegate in order to use table view
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [[String:Any]]() //creates an array of dictionaries
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+        print("Hello")
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+            //this has data:
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+              self.movies = dataDictionary["results"] as! [[String:Any]] //casting
+              self.tableView.reloadData()
+              print(dataDictionary)
+              // TODO: Get the array of movies
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
+
+           }
+        }
+        task.resume()
+
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCellTableViewCell") as! MovieCellTableViewCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String //see in API
+        
+        //cell.textLabel!.text = "row: \(indexPath.row)" // \(variable) will print/put varaible in a string
+        //add an exclamation point as Swift optional
+    
+        cell.titleLabel.text = title
+        
+        let synopsis = movie["overview"] as! String //see API
+        cell.synopsisLabel.text = synopsis
+        //put 0 lines in label configuration and it will grow to however many lines you need
+        
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+        cell.posterView.af_setImage(withURL: posterUrl)
+        return cell
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
