dde877fbb6805c764f6134e93f17f706f58a6518
Brylee Pavlik <pavlikbr@msu.edu>
2021-02-21 21:08:25 -0500

    Added Part 2 (Unit 2) content

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Brylee Pavlik on 2/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
